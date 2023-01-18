d8f78921f367e5d5f8e96c24e86312ad6046d7e8
ajeer333 <ajeer333@gmail.com>
2023-01-18 14:54:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-const getBooks=async()=>{
-    try{
-    let res=await fetch('https://striveschool-api.herokuapp.com/books')
-    let data=await res.json()
-   // console.log(data)
-        let container=document.getElementById('booksGoHere')
+
+function getBooks(data){
+    let container=document.getElementById('booksGoHere')
         let arrayOfBooks=data.map((singleBook)=>{
             return `<div class="col-6 col-md-3 mb-3">
             <div class="card" >
@@ -11,7 +8,7 @@ const getBooks=async()=>{
             <div class="card-body">
               <h5 class="card-title">${singleBook.title}</h5>
               <div class="d-flex justify-center-between">
-              <button type="button" class="btn btn-outline-dark cart" >Add to cart</button>
+              <button type="button" class="btn btn-outline-dark cart" onclick="addToCart()" >Add to cart</button>
               <button type="button" class="btn btn-outline-dark ml-auto">Skip</button>
               </div>
             </div>
@@ -19,6 +16,18 @@ const getBooks=async()=>{
           </div>`
         }).join("")
         container.innerHTML=arrayOfBooks
+}
+
+
+
+
+
+const fetchBooks=async()=>{
+    try{
+    let res=await fetch('https://striveschool-api.herokuapp.com/books')
+    let data=await res.json()
+   // console.log(data)
+        getBooks(data)
         
     }
     catch(error){
@@ -26,24 +35,13 @@ const getBooks=async()=>{
     }
 
 }
-window.onload=()=>{
-getBooks()
+window.onload=async ()=>{
+await fetchBooks()
+
 }
 
 
 
-function addToCart(){
-
-        let btns=document.querySelectorAll('.cart')
-        console.log(btns)
-//console.log(btns)
-//btns.forEach((btn) => {
-    //console.log(btn)
-    //btn.closest('.card').innerHTML+=`<span class="badge badge-danger">Added to cart</span>`
-//});
- 
-   
-}
 
 
-addToCart()
+
